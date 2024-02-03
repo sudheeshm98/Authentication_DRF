a0b85d6881d7a0a8ecf9438a4069209629535933
Sudheesh M <vpzsudheesh@gmail.com>
2024-02-03 15:41:13 +0530

    commit

--- userprofile/account/views.py
+++ userprofile/account/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import User
 from .serializer import RegisterSerializer,UserSerializer
-from rest_framework import generics
+from rest_framework import generics, status
+
 
 # Create your views here.
 
-class RegisterView(generics.CreateAPIView):
-    def post(self,request):
-        serializer = self.serializer_class(data=request.data)
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            token,created = Token.object.get_or_create(user=user)
-            return Response(status=status)
+            return Response({'message': 'User registered Successfully.'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
